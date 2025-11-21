011fef46de1f53afb675fdae1cfe4d4f97f33529
derekrnadeau <150065746+derekrnadeau@users.noreply.github.com>
2025-11-21 09:16:44 -0500

    Create sofa.py

---
+++ librosa/sofa.py
@@ -0,0 +1,227 @@
+import librosa 
+import numpy as np
+import math
+import netCDF4 as ncdf
+import argparse
+import sys
+import re
+
+parser = argparse.ArgumentParser(
+    prog="Script to load, read, and utilize SOFA files",
+    description="",
+)
+parser.add_argument('-f',   '--sofapath'    )
+parser.add_argument('-i',   '--inputpath'  )
+parser.add_argument('-ele', '--elevation'   )
+parser.add_argument('-azi', '--azimuth'     )
+parser.add_argument('-o',   '--outputpath'  )
+parser.add_argument('-xtc', '--crosstalkx'  )
+args = parser.parse_args()
+
+# print(args)
+
+if (not args.outputpath): 
+    # TODO generate output file name and put it in same folder as input file
+    print("No output path provided. File will be created at: ")
+
+if (not args.crosstalkx):
+    args.crosstalkx = False
+
+try:
+    if (not args.sofapath): raise ValueError
+except:
+    sys.exit("InputError: No SOFA file provided.")
+
+try:
+    if (not args.elevation): raise ValueError
+except:
+    sys.exit("InputError: No elevation provided.")
+
+try:
+    if (not args.azimuth): raise ValueError
+except:
+    sys.exit("InputError: No azimuth provided.")
+
+try:
+    args.elevation = float(args.elevation)
+    assert isinstance(args.elevation, float)
+except:
+    sys.exit("InputError: 'elevation' is not a valid float.")
+
+try:
+    args.azimuth = float(args.azimuth)
+    assert isinstance(args.azimuth, float)
+except:
+    sys.exit("InputError: 'azimuth' is not a valid float.")
+
+try: 
+    if (not re.match('^(.+[\\/])?[^\\/]+\.sofa$', args.sofapath)): raise ValueError
+except:
+    sys.exit("InputError: Path to SOFA is invalid.")
+
+### SOURCES:
+# https://unidata.github.io/netcdf4-python/
+# https://www.mathworks.com/help/simulink/ref_extras/sphericaltocartesian.html
+# https://docs.python.org/3/library/argparse.html
+
+## TODO: command line 
+## TODO: integrate with librosa 
+
+file_path = '/Users/dereknadeau/3DA_Nadeau_SOFA/D1_48K_24bit_0.3s_FIR_SOFA.sofa'    # smol
+# file_path = '/Users/dereknadeau/3DA_Nadeau_SOFA/D1_48K_24bit_256tap_FIR_SOFA.sofa'  # bigg
+
+# M = number of measurements taken (combinations of azimuth and elevation)
+# C = coordinate system in 3D
+# I = singleton dimension, defines scalar value (one sampling rate, one listener position in C coords)
+# N = num samples per measurement 
+# R = num receivers (should always be two ears)
+# E = num emitters (should always be one speaker)
+
+class Measurement: 
+    def __init__(self, IR, position, N):
+        self.N = N
+        self.L = IR[0]
+        self.R = IR[1]
+        self.azimuth    = position[0]
+        self.elevation  = position[1]
+        self.distance   = position[2]
+
+class SOFA: 
+    def __init__(self, file_path):
+        self.sofa = ncdf.Dataset(file_path, 'r', format='NETCDF4')   # load file
+        assert len(self.sofa.variables['ReceiverPosition'][:]) == 2       # you need two ears!
+        assert len(self.sofa.variables['EmitterPosition'][:])  == 1       # you need one speaker!
+
+        self.sr = self.sofa.variables['Data.SamplingRate'][0]             # get sampling rate
+        self.I = 1
+        self.C = 3
+        self.M = self.sofa.dimensions['M'].size
+        self.N = self.sofa.dimensions['N'].size
+
+        self.measurements = [
+            Measurement(
+                self.sofa.variables['Data.IR'][i], 
+                self.sofa.variables['SourcePosition'][i], 
+                self.N)
+            for i in range(self.M)]
+        
+    def get_IR(self, target_azi, target_ele): 
+
+        # find the closest angle measurement taken available in the file!
+        ### could definitely be optimized with more eloquent python
+        ### I wasn't familiar with the math, so I left it more explicit 
+
+        # in a loop of all IR measurements
+        # measure distance between IR azi/ele and target azi/ele 
+            # convert from spherical to cartesian coordinates
+        # keep track of the measurement index with the smallest distance
+
+        min_distance        = math.inf
+        min_distance_index  = -1
+
+        target_azi  = np.deg2rad(target_azi)
+        target_ele  = np.deg2rad(target_ele)
+
+        # we assume distance is constant, and therefore a consistent unit sphere radius of 1 
+        # theta is traditionally azimuth, phi is traditionally elevation 
+
+        # x = r × sin(phi) × cos(theta)
+        # y = r × sin(phi) × sin(theta)
+        # z = r × cos(phi)
+        target_x = math.sin(target_ele) * math.cos(target_azi)
+        target_y = math.sin(target_ele) * math.sin(target_azi)
+        target_z = math.cos(target_ele)
+
+        for i in range(self.M): 
+            curr_azi    = np.deg2rad(self.measurements[i].azimuth)
+            curr_ele    = np.deg2rad(self.measurements[i].elevation)
+            
+            curr_x = math.sin(curr_ele) * math.cos(curr_azi)
+            curr_y = math.sin(curr_ele) * math.sin(curr_azi)
+            curr_z = math.cos(curr_ele)
+
+            # distances between two points in cartesian 3D 
+            curr_distance = math.sqrt(
+                math.pow(target_x - curr_x, 2) +
+                math.pow(target_y - curr_y, 2) + 
+                math.pow(target_z - curr_z, 2)
+            )
+
+            # update best fit if applicable
+            if curr_distance < min_distance:
+                min_distance = curr_distance
+                min_distance_index = i
+            
+            # if ever a perfect match, return immediately
+            if curr_distance == 0:
+                print(
+                    "Exact match! Using IR with azi & ele: ", 
+                    self.measurements[min_distance_index].azimuth, 
+                    ' - ',
+                    self.measurements[min_distance_index].elevation
+                )
+                return self.measurements[min_distance_index]
+
+        print(
+            "No exact match. Using IR with azi & ele: ", 
+            self.measurements[min_distance_index].azimuth, 
+            ' - ',
+            self.measurements[min_distance_index].elevation, 
+            " with distance: ", min_distance
+        )
+        return self.measurements[min_distance_index]
+
+
+
+
+
+
+        
+
+mySofa = SOFA(file_path)
+
+mySofa.get_IR(args.azimuth, args.elevation)
+
+
+
+
+
+# <class 'netCDF4.Dataset'>
+# root group (NETCDF4 data model, file format HDF5):
+#     dimensions(sizes):        I(1), C(3), R(2), E(1), N(14400), M(50), S(0)
+#     variables(dimensions):    float64 ListenerPosition(I, C), 
+#                               float64 ReceiverPosition(R, C, I), 
+#                               float64 SourcePosition(M, C), 
+#                               float64 EmitterPosition(E, C, I), 
+#                               float64 ListenerUp(I, C), 
+#                               float64 ListenerView(I, C), 
+#                               float64 EmitterUp(E, C, I), 
+#                               float64 EmitterView(E, C, I), 
+#                               float64 Data.IR(M, R, E, N), //// 50 measurements, 2 receivers/ears, 1 emitter, n samples 
+#                               float64 Data.SamplingRate(I), 
+#                               float64 Data.Delay(I, R, E)
+#     Conventions:              SOFA
+#     Version:                  1.0
+#     SOFAConventions:          MultiSpeakerBRIR
+#     SOFAConventionsVersion:   0.3
+#     APIName:                  ARI SOFA API for Matlab/Octave
+#     APIVersion:               1.1.1
+#     ApplicationName:          MATLAB
+#     ApplicationVersion:       R2018a
+#     AuthorContact:            gavin.kearney@york.ac.uk
+#     Comment:                  50 source positions. KU100 subject. Measurement utlized Genelec 8030/8040 Loudspeakers and 3s swept sine technique. Reciever microphones were KU100 built in mics via MOTU UltraLite-mk3 Hybrid Preamps. Filters were free field equalized for the microphones responces. (approx.) Linear phase BRIRs.
+#     DataType:                 FIRE
+#     History:                  Measurement, Microphone Free Field Equalization, Trim
+#     License:                  Copyright 2018, University of York, Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+#     Organization:             AudioLab, Department of Electronic Engineering, University of York, United Kingdom.
+#     References:               https://doi.org/10.3390/app8112029
+#     RoomType:                 reverberant
+#     Origin:                   Exponential Swept Sine Wave Measuremnt
+#     DateCreated:              2019-08-01 13:21:06
+#     DateModified:             2020-05-13 21:33:31
+#     Title:                    D1 BRIRs
+#     DatabaseName:             SADIE II
+#     ListenerShortName:        D1
+#     RoomDescription:          Acoustically Treated Listening Environment
+#     NCProperties:             version=1|netcdflibversion=4.6.1|hdf5libversion=1.8.12
+#     groups: 
